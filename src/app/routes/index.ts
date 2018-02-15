2b5be9d6a58ddd77257961068e1efa1ecf1ae166
Passakorn Rattanaprapan <passakorn_working@hotmail.com>
2018-02-15 20:45:31 +0700

    Add route

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,11 +1,19 @@
 import auth from './auth.route'
 import user from './user.route'
 import project from './project.route'
+import member from './member.route'
+import database from './database.route'
+import folder from './folder.route'
+import endpoint from './endpoint.route'
 import position from './project-position.route'
 
 export {
     auth,
     user,
     project,
+    member,
+    database,
+    folder,
+    endpoint,
     position
 }
