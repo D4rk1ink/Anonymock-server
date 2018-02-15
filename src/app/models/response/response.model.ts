f2bde94c07ada9a395e6f5a8bb92f409d2a3dcbe
Passakorn Rattanaprapan <passakorn_working@hotmail.com>
2018-02-16 01:59:52 +0700

    Add response model

---
+++ src/app/models/response/response.model.ts
@@ -0,0 +1,51 @@
+import { Model, Document, model, Error } from 'mongoose'
+import ResponseSchema from './response.schema'
+import { Project } from '../project'
+
+interface IResponseModel extends Document {
+    id: string
+    name: string
+    environment: string
+    condition: any
+    response: any
+    endpoint: string
+}
+
+const ResponseModel = model<IResponseModel>('Response', ResponseSchema)
+
+export class Response {
+
+    static getModel () {
+        return ResponseModel
+    }
+
+    static async create (newResponse) {
+        return new ResponseModel(newResponse).save()
+    }
+
+    static async update (id, update) {
+        return await ResponseModel.findByIdAndUpdate(id, update)
+    }
+
+    static async remove (id) {
+        return await ResponseModel.findByIdAndRemove(id)
+    }
+
+    static async findById (id, fields = '') {
+        return await ResponseModel.findById(id, fields)
+            .populate('method', 'id name')
+    }
+
+    static async findOne (condition) {
+        return await ResponseModel.findOne(condition)
+            .populate('method', 'id name')
+    }
+
+    static async findAll (condition = {}, fields = '') {
+        return await ResponseModel.find(condition, fields)
+    }
+
+    static async search (endpoint, search, fields = '') {
+        return await ResponseModel.find({ endpoint: endpoint, name: new RegExp(search, 'gi') }, fields)
+    }
+}
