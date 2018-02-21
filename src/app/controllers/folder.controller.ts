b99100243bcf12abd13de6162301b6d0906b0972
Passakorn Rattanaprapan <passakorn_working@hotmail.com>
2018-02-21 11:43:09 +0700

    Add try catch exception

--- src/app/controllers/folder.controller.ts
+++ src/app/controllers/folder.controller.ts
@@ -39,16 +39,20 @@ export const create = async (req: Request, res: Response) => {
 
 export const getById = async (req: Request, res: Response) => {
     if (await verify.verifyAdmin(req, res) || verify.verifyMember(req, res)) {
-        const id = req.params.id
-        const myFolder = await Folder.findById(id, 'id name')
-        if (myFolder) {
-            const data = {
-                id: myFolder.id,
-                name: myFolder.name
+        try {
+            const id = req.params.id
+            const myFolder = await Folder.findById(id, 'id name')
+            if (myFolder) {
+                const data = {
+                    id: myFolder.id,
+                    name: myFolder.name
+                }
+                res.json(preResponse.data(data))
+            } else {
+                res.json(preResponse.error(null, 'Folder not found'))
             }
-            res.json(preResponse.data(data))
-        } else {
-            res.json(preResponse.error(null, 'Folder not found'))
+        } catch (err) {
+            res.json(preResponse.error(null, 'Get fail'))
         }
     } else {
         res
@@ -59,30 +63,34 @@ export const getById = async (req: Request, res: Response) => {
 
 export const search = async (req: Request, res: Response) => {
     if (await verify.verifyAdmin(req, res) || verify.verifyMember(req, res)) {
-        const { project, search, page, all } = req.query
-        const myFolders = await Folder.search(project, search)
-        if (myFolders) {
-            let sliceFolders: any[] = []
-            const itemPerPage = 10
-            if (all) {
-                sliceFolders = myFolders
-            } else {
-                sliceFolders = myFolders.slice((page - 1 * itemPerPage), page * itemPerPage)
-            }
-            const folders = sliceFolders.map(folder => {
-                return {
-                    id: folder.id,
-                    name: folder.name,
-                    countEndpoints: folder.endpoints.length
+        try {
+            const { project, search, page, all } = req.query
+            const myFolders = await Folder.search(project, search)
+            if (myFolders) {
+                let sliceFolders: any[] = []
+                const itemPerPage = 10
+                if (all) {
+                    sliceFolders = myFolders
+                } else {
+                    sliceFolders = myFolders.slice((page - 1 * itemPerPage), page * itemPerPage)
                 }
-            })
-            const data = {
-                folders: folders,
-                limitPage: all ? 0 : Math.ceil(myFolders.length / itemPerPage)
+                const folders = sliceFolders.map(folder => {
+                    return {
+                        id: folder.id,
+                        name: folder.name,
+                        countEndpoints: folder.endpoints.length
+                    }
+                })
+                const data = {
+                    folders: folders,
+                    limitPage: all ? 0 : Math.ceil(myFolders.length / itemPerPage)
+                }
+                res.json(preResponse.data(data))
+            } else {
+                res.json(preResponse.error(null, 'Folder not found'))
             }
-            res.json(preResponse.data(data))
-        } else {
-            res.json(preResponse.error(null, 'Project not found'))
+        } catch (err) {
+            res.json(preResponse.error(null, 'Search fail'))
         }
     } else {
         res
