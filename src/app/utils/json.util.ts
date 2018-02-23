4e07d839d3df33ade948e35e19208a9c1649a6f2
Passakorn Rattanaprapan <passakorn_working@hotmail.com>
2018-02-23 23:50:57 +0700

    Refector json util

--- src/app/utils/json.util.ts
+++ src/app/utils/json.util.ts
@@ -2,24 +2,12 @@ export const isJSON = (json) => {
     return typeof json === 'object' && !Array.isArray(json)
 }
 
-export const to = (json) => {
-    return typeof json === 'object' && !Array.isArray(json)
-}
-
-export const hasProperties = (data, correct) => {
-    
-}
-
 export const containCompare = (data, correctData) => {
-    // if (Object.keys(correctData).length > 0) {
-        for (const key in correctData) {
-            if (!data.hasOwnProperty(key) || data[key] !== correctData[key]) {
-                return false
-            }
+    for (const key in correctData) {
+        if (!data.hasOwnProperty(key) || data[key] !== correctData[key]) {
+            return false
         }
-    // } else {
-    //     return false
-    // }
+    }
     return true
 }
 
@@ -39,23 +27,7 @@ export const deepCompare = (data, correctData) => {
         }
     } else if (typeof data === 'object') {
         for (const key in data) {
-            if (correctData.hasOwnProperty(key)) {
-                if (Array.isArray(data[key])) {
-                    if (data[key].length !== correctData[key].length) {
-                        return false
-                    } else {
-                        for (const i of data[key].keys()) {
-                            if (!deepCompare(data[key][i], correctData[key][i])) {
-                                return false
-                            }
-                        }
-                    }
-                } else {
-                    if (!deepCompare(data[key], correctData[key])) {
-                        return false
-                    } 
-                }
-            } else {
+            if (!deepCompare(data[key], correctData[key])) {
                 return false
             }
         }
