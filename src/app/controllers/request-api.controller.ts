865a374f64fa4b299e8c7b671ce5a5cac6b92445
Passakorn Rattanaprapan <passakorn_working@hotmail.com>
2018-02-24 18:34:00 +0700

    Update return response condition and map database

--- src/app/controllers/request-api.controller.ts
+++ src/app/controllers/request-api.controller.ts
@@ -35,37 +35,154 @@ export const request = async (req: Request, res: Response) => {
             }
             let hasCorrect = false
             for (const response of myEndpoint.responses) {
-                const paramsCorrect = json.deepCompare(params, response.condition.params)
-                const headersCorrect = json.containCompare(req.headers, response.condition.headers)
-                const queryStringCorrect = json.deepCompare(req.query, response.condition.queryString)
-                let bodyCorrect = true
+                response.condition.params = mapEnvironment(response.condition.params, myProject.environments)
+                response.condition.body = mapEnvironment(response.condition.body, myProject.environments)
+                response.condition.headers = mapEnvironment(response.condition.headers, myProject.environments)
+                response.condition.queryString = mapEnvironment(response.condition.queryString, myProject.environments)
+                
+                response.response.headers = mapEnvironment(response.response.headers, myProject.environments)
+                response.response.body = mapEnvironment(response.response.body, myProject.environments)
+
+                // const paramsCorrect = json.deepCompare(params, response.condition.params)
+                // const headersCorrect = json.containCompare(req.headers, response.condition.headers)
+                // const queryStringCorrect = json.deepCompare(req.query, response.condition.queryString)
+                
+                const extractParamsDbToken = extractDbToken(params, response.condition.params)
+                const extractHeadersDbToken = extractDbToken(req.headers, response.condition.headers)
+                const extractQueryStringDbToken = extractDbToken(req.query, response.condition.queryString)
+       
+                let extractBodyDbToken = []
                 if (method !== 'GET') {
-                    json.containCompare(req.body, response.condition.body)
+                    extractBodyDbToken = extractDbToken(req.body, response.condition.body)
                 }
-                if (paramsCorrect && headersCorrect && queryStringCorrect && bodyCorrect) {
+                console.log(extractParamsDbToken, extractHeadersDbToken, extractQueryStringDbToken, extractBodyDbToken)
+                if (extractParamsDbToken && extractHeadersDbToken && extractQueryStringDbToken && extractBodyDbToken) {
+                    const dbTokens = [
+                        ...extractParamsDbToken,
+                        ...extractHeadersDbToken,
+                        ...extractQueryStringDbToken,
+                        ...extractBodyDbToken
+                    ]
+                    const dbSelected = filterDababase(dbTokens, myProject.database.data)
+                    const responseBody = dbSelected.map(db => mapDatabase(response.response.body, db))
                     setTimeout(() => {
                         res
                             .status(response.response.statusCode)
                             .header(response.response.headers)
-                            .json(response.response.body)
+                            .json(responseBody)
                     }, response.response.delay)
                     hasCorrect = true
                     break
                 }
             }
             if (!hasCorrect) {
-                res
-                    .status(404)
-                    .json({})
+                res.status(404).json({a:'asd'})
             }
         } else {
-            res
-                .status(404)
-                .json({})
+            res.status(404).json({e:'Endpoint not found'})
         }
     } else {
-        res
-                .status(404)
-                .json({})
+        res.status(404).json({e:'Project not found'})
     }
+}
+
+const mapEnvironment = (template, environment) => {
+    if (typeof template !== 'string') {
+        template = JSON.stringify(template)
+    }
+    const regex_token = /{{\s*\$env.([^}}\s]+)\s*}}/g
+    template = template.replace(regex_token, (match, capture) => {
+        return environment[capture]
+    })
+    return JSON.parse(template)
+}
+
+const mapDatabase = (template, database) => {
+    if (typeof template !== 'string') {
+        template = JSON.stringify(template)
+    }
+    const regex_token = /{{\s*\$db.([^}}\s]+)\s*}}/g
+    
+    template = template.replace(regex_token, (match, capture) => {
+        const keys = capture.split('.')
+        let nested = database
+        for (const key of keys) {
+            nested = nested[key]
+        }
+        return nested
+    })
+    return JSON.parse(template)
+}
+
+const extractDbToken = (data, correctData) => {
+    let temp: any = []
+    const regex_token = /{{\s*\$db.([^}}\s]+)\s*}}/g
+    if (Array.isArray(data) !== Array.isArray(correctData)) {
+        return false
+    }
+    if (Array.isArray(correctData)) {
+        if (data.length !== correctData.length) {
+            return false
+        } else {
+            for (const i of correctData.keys()) {
+                const a = extractDbToken(data[i], correctData[i])
+                if (Array.isArray(a)) {
+                    temp = [...a, ...temp]
+                }
+                if (a === false) {
+                    return false
+                }
+            }
+        }
+    } else if (typeof correctData === 'object') {
+        for (const key in correctData) {
+            if (!data.hasOwnProperty(key)) {
+                return false
+            }
+            const a = extractDbToken(data[key], correctData[key])
+            if (Array.isArray(a)) {
+                temp = [...a, ...temp]
+            }
+            if (a === false) {
+                return false
+            }
+        }
+    } else {
+        const regex_token = /{{\s*\$db.([^}}\s]+)\s*}}/g
+        if (new RegExp(regex_token).test(correctData)) {
+            const key = (new RegExp(regex_token).exec(correctData) || []).slice(1).pop()
+            const value = data
+            return [{
+                key: key,
+                value: value
+            }]
+        } else {
+            if (data !== correctData) {
+                return false
+            }
+        }
+    }
+    return temp
+}
+
+const filterDababase = (conditions, db) => {
+    let selected: any[] = []
+    for (const data of db) {
+        let isCorrect = true
+        for (const condition of conditions) {
+            const keys = condition.key.split('.')
+            let nested = data
+            for (const key of keys) {
+                nested = nested[key]
+            }
+            if (nested != condition.value) {
+                isCorrect = false
+                break
+            }
+        }
+        if (isCorrect) {
+            selected.push(data)
+        }
+    }
+    return selected
 }
