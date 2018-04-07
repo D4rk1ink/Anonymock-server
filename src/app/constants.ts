e60f774f94c3cc7d342b0c1b2d1b017cf4a7116e
Passakorn Rattanaprapan <passakorn_working@hotmail.com>
2018-04-08 00:59:04 +0700

    Set defult profile picture

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -16,3 +16,4 @@ export const DB_URL: any = process.env.DB_URL
 export const INIT_USERNAME: any = process.env.INIT_USERNAME
 export const INIT_EMAIL: any = process.env.INIT_EMAIL
 export const INIT_PASSWORD: any = process.env.INIT_PASSWORD
+export const DEFAULT_PROFILE_PICTURE: any = process.env.DEFAULT_PROFILE_PICTURE
